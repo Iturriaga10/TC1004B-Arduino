5630afd232d29f1a2c705042cdaba2b99f7594e9
NephilR7 <jrosalesm43771r@gmail.com>
2021-11-29 13:01:04 -0600

    Aruino uno wifi code for sending sensors data to Db

--- Prueba_Wifi/Prueba_Wifi.ino
+++ Prueba_Wifi/Prueba_Wifi.ino
@@ -1,14 +1,12 @@
-#include <WiFiNINA.h>
 #include <ArduinoJson.h>
+#include <WiFiNINA.h>
 
-char ssid = "SoteloNetwork";
-char password = "1turr1aga";
+char ssid[] = "SoteloNetwork";
+char password[] = "1turr1aga";
 
 int status = WL_IDLE_STATUS;
 
-char server[] = "https://my-json-server.typicode.com/typicode/demo";
-
-String postData;
+char server[] = "1711-187-189-127-57.ngrok.io";    //Always modify when re-run ngrok
 
 WiFiClient client;
 
@@ -16,13 +14,13 @@ void setup(){
   Serial.begin(9600);
 
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to Network: ");
+    Serial.println("Attempting to connect to Network: ");
     Serial.println(ssid);
     status = WiFi.begin(ssid,password);
     delay (1000);
   }
 
-  Serial.print("SSID: ");
+  Serial.print("Connected to SSID: ");
   Serial.println(WiFi.SSID());
   IPAddress ip = WiFi.localIP();
   IPAddress gateway = WiFi.gatewayIP();
@@ -33,34 +31,69 @@ void setup(){
 
 void loop(){
 
-  DynamicJsonDocument doc(1024);
+  DynamicJsonDocument doc1(1024);
+  DynamicJsonDocument doc2(1024);
+  DynamicJsonDocument doc3(1024);
   
-  int sensor1 = analogRead(A0);
-  int sensor2 = analogRead(A1);
-  int sensor3 = analogRead(A2);
+  float sensor1 = analogRead(A0);
+  float sensor2 = analogRead(A1);
+  float sensor3 = analogRead(A2);
 
-  doc["Sensor 1"] = sensor1;
-  doc["Sensor 2"] = sensor2;
-  doc["Sensor 3"] = sensor3;
+  doc1["data_measured"] = sensor1;
+  doc1["sensor"] = "1";
 
-  postData = serializeJson(doc, Serial);
+  doc2["data_measured"] = sensor2;
+  doc2["sensor"] = "2";
+
+  doc3["data_measured"] = sensor3;
+  doc3["sensor"] = "3";
+
+  String postData1;
+  String postData2;
+  String postData3;
+
+  serializeJson(doc1, postData1);
+  serializeJson(doc2, postData2);
+  serializeJson(doc3, postData3);
 
    if (client.connect(server, 80)) {
-    client.println("POST /posts/post.php HTTP/1.1");
-    client.println("Host: https://my-json-server.typicode.com/typicode/demo");
-    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("POST /pushData HTTP/1.1");
+    client.println("Host: 1711-187-189-127-57.ngrok.io");
+    client.println("Content-Type: application/json");
     client.print("Content-Length: ");
-    client.println(postData.length());
+    client.println(postData1.length());
     client.println();
-    client.print(postData);
+    client.print(postData1);
+   }
+
+   if (client.connect(server, 80)) {
+    client.println("POST /pushData HTTP/1.1");
+    client.println("Host: 1711-187-189-127-57.ngrok.io");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(postData2.length());
+    client.println();
+    client.print(postData2);
+   }
+
+   if (client.connect(server, 80)) {
+    client.println("POST /pushData HTTP/1.1");
+    client.println("Host: 1711-187-189-127-57.ngrok.io");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(postData3.length());
+    client.println();
+    client.print(postData3);
   }
 
   if (client.connected()) {
     client.stop();
   }
   
-  Serial.println(postData);
+  Serial.println(postData1);
+  Serial.println(postData2);
+  Serial.println(postData3);
 
-  delay(3000);
+  delay(20000);
   
 }
